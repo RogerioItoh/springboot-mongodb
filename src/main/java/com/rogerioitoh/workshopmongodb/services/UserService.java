89ce2dd05bafe6e2be12d51fafc700b95f95de5c
Rogerio Itoh <rogerioitoh@gmail.com>
2021-10-06 12:03:41 -0300

    Atualização de usuário com PUT

--- src/main/java/com/rogerioitoh/workshopmongodb/services/UserService.java
+++ src/main/java/com/rogerioitoh/workshopmongodb/services/UserService.java
@@ -47,7 +47,21 @@ public class UserService {
 	}
 	
 	
+	public User update (User obj)
+	{
+		User newObj = repo.findById(obj.getId()).orElse(null); 
+		updateDate(newObj , obj);
 	
+		return repo.save(newObj);
+	}
+	
+	
+	private void updateDate(User newObj, User obj) {
+		
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO (UserDTO objDto)
 	{
 		return new User(objDto.getId(), objDto.getName() , objDto.getEmail());
