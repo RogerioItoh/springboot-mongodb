05a65678c74a6f0dd86039ccf5d925eb9709a80e
Rogerio Itoh <rogerioitoh@gmail.com>
2021-10-06 11:48:18 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/rogerioitoh/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/rogerioitoh/workshopmongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.rogerioitoh.workshopmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rogerioitoh.workshopmongodb.domain.User;
 import com.rogerioitoh.workshopmongodb.dto.UserDTO;
@@ -38,5 +41,24 @@ public class UserResource {
 	}
 
 	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		
+		service.delete(id);				
+		return ResponseEntity.noContent().build();
+
+	}
+
+	
+	
+	
+
 }
--- src/main/java/com/rogerioitoh/workshopmongodb/services/UserService.java
+++ src/main/java/com/rogerioitoh/workshopmongodb/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rogerioitoh.workshopmongodb.domain.User;
+import com.rogerioitoh.workshopmongodb.dto.UserDTO;
 import com.rogerioitoh.workshopmongodb.repository.UserRepository;
 import com.rogerioitoh.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -26,6 +27,30 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	
+	public User insert (User obj)
+	{
+		return repo.insert(obj);
+	}
+	
+	
+	public void delete (String id)
+	{
+		User user = repo.findById(id).orElse(null);
+		if (user == null)
+		{
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+				
+		repo.deleteById(id);
+	}
+	
+	
+	
+	public User fromDTO (UserDTO objDto)
+	{
+		return new User(objDto.getId(), objDto.getName() , objDto.getEmail());
 	}
 	
 	
